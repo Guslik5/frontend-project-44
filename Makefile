de9b803d66de972d1731c565545be85212436aa3
Guslik5 <guslik2003@mail.ru>
2024-05-14 18:08:48 +0300

    4 task project

--- Makefile
+++ Makefile
@@ -5,3 +5,5 @@ install:
 brain-games:
 	node bin/brain-games.js
 
+lint:
+	npx eslint
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,6 +2,6 @@ import getName from '../src/cli.js';
 
 console.log("Welcome to the Brain Games!")
 
-const name = getName();
+const  name =  getName();
 
 console.log(`Hello, ${name}!`)
